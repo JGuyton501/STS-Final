486ba4a17cbe3ee914d6cd630f95171b4a0aae4b
Justin Guyton <guyton.justin@gmail.com>
2017-08-19 20:11:12 -0500

    quiz view update for admin

--- client/src/components/Quiz.js
+++ client/src/components/Quiz.js
@@ -29,9 +29,9 @@ class Quizzes extends React.Component {
 			        <div className="col s12 m4">
 			          <div className="card">
 			            <div className="card-content">
-			              <a href="#"><span className="card-title">Quiz 1</span></a>
-			              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-			              labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			              <span className="card-title">Quiz 1</span>
+			              <p>Quiz description if you are only a student employee</p>
+			              <br/>
 			              <p>Type: 3D</p>
 			              
 			            </div>
@@ -46,9 +46,12 @@ class Quizzes extends React.Component {
 			        <div className="col s12 m4">
 			          <div className="card">
 			            <div className="card-content">
-			              <a href="#"><span className="card-title">Quiz 2</span></a>
-			              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
-			              labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			              <a href=" ">
+			              	<span className="card-title col s12 m6" style={{padding: 0}}>Quiz 2</span>
+			              	<span className="card-title col s12 m6 right-align" style={{padding: 0}}><i className="mini material-icons">keyboard_arrow_right</i></span>
+			              </a>
+			              <p>Quiz if you are an one of those manager people</p>
+			              <br/>
 			              <p>Type: 3D</p>
 			              
 			            </div>
@@ -64,9 +67,10 @@ class Quizzes extends React.Component {
 			        <div className="col s12 m4">
 			          <div className="card">
 			            <div className="card-content">
-			              <a href="#"><span className="card-title">Quiz 3</span></a>
+			              <span className="card-title">Quiz 3</span>
 			              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut 
 			              labore et dolore magna aliqua. Ut enim ad minim veniam</p>
+			              <br/>
 			              <p>Type: 3D</p>
 			              
 			            </div>
